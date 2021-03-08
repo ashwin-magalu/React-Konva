a4b1e73d4a15049c664ca9c86a4230f90fb2d5dd
Ashwin M A <58284442+ashwin-magalu@users.noreply.github.com>
2021-03-08 16:40:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ChangeZIndex from "./components/ChangeZIndex";
 import CollisionDetection from "./components/CollisionDetection";
 import CustomShapes from "./components/CustomShapes";
 import DragAndDrop from "./components/DragAndDrop";
+import DragAndSelectShape from "./components/DragAndSelectShape";
+import DragAndSelectShapeNormal from "./components/DragAndSelectShapeNormal";
 import DropImage from "./components/DropImage";
 import ExportImage from "./components/ExportImage";
 import FreeDrawing from "./components/FreeDrawing";
@@ -15,12 +17,20 @@ import GuideLine from "./components/GuideLine";
 import ImageResize from "./components/ImageResize";
 import Mirror from "./components/Mirror";
 import ModifyCurves from "./components/ModifyCurves";
+import MultipleShapesAndSelectAndResize from "./components/MultipleShapesAndSelectAndResize";
+import MultipleShapesDragSelectAndResize from "./components/MultipleShapesDragSelectAndResize";
+import Path from "./components/Path";
+import PathReact from "./components/PathReact";
+import PathWithSelect from "./components/PathWithSelect";
+import RectangleAndTextSelectAndResize from "./components/RectangleAndTextSelectAndResize";
+import RectangleSelectAndResize from "./components/RectangleSelectAndResize";
 import ResizeAndRotate from "./components/ResizeAndRotate";
 import SavePdf from "./components/SavePdf";
 import SelectAndResize from "./components/SelectAndResize";
 import StraightLine from "./components/StraightLine";
 import StraightLine2 from "./components/StraightLine2";
 import StraightLineWithSlice from "./components/StraightLineWithSlice";
+import TransformImage from "./components/TransformImage";
 import UndoRedo from "./components/UndoRedo";
 import WheelZoom from "./components/WheelZoom";
 import ZIndex from "./components/ZIndex";
@@ -49,11 +59,21 @@ function App() {
       {/* <ImageResize /> */}
       {/* <ModifyCurves /> */}
       {/* <CollisionDetection /> */}
-      {/* <WheelZoom /> */}
+      <WheelZoom />
       {/* <SavePdf /> */}
       {/* <StraightLine /> */}
       {/* <StraightLine2 /> */}
-      <StraightLineWithSlice />
+      {/* <StraightLineWithSlice /> */}
+      {/* <Path /> */}
+      {/* <PathReact /> */}
+      {/* <PathWithSelect /> */}
+      {/* <DragAndSelectShapeNormal /> */}
+      {/* <DragAndSelectShape /> */}
+      {/* <RectangleSelectAndResize /> */}
+      {/* <RectangleAndTextSelectAndResize /> */}
+      {/* <TransformImage /> */}
+      {/* <MultipleShapesAndSelectAndResize /> */}
+      {/* <MultipleShapesDragSelectAndResize /> */}
     </div>
   );
 }
